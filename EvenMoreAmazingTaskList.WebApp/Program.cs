4468c31ef868ff8d77171eb758a9b45f0732084f
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-11-16 22:29:44 -0300

    Amazing but simple Task List manager.

--- EvenMoreAmazingTaskList.WebApp/Program.cs
+++ EvenMoreAmazingTaskList.WebApp/Program.cs
@@ -11,6 +11,7 @@ namespace EvenMoreAmazingTaskList.WebApp
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
