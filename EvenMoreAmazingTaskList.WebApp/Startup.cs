750c32abeb84df59b819def885edc2d516bb9750
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-11-16 22:33:49 -0300

    Changed the default route to TaskItems controller.

--- EvenMoreAmazingTaskList.WebApp/Startup.cs
+++ EvenMoreAmazingTaskList.WebApp/Startup.cs
@@ -52,7 +52,7 @@ namespace EvenMoreAmazingTaskList.WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=TaskItems}/{action=Index}/{id?}");
             });
         }
     }
